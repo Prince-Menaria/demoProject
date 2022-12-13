da95492439cd7e9a5c52dee7d41114c62baa1953
prince-menaria <princemenaria04@gmail.com>
2022-12-13 10:58:24 +0530

    initial substract

---
+++ src/basicProgram/substract.java
@@ -0,0 +1,13 @@
+package basicProgram;
+
+public class substract {
+	
+	public static void main(String[] args) {
+		int a = 40;
+		int b = 20;
+		 int c ;
+		 c = a- b;
+		 System.out.println(c);
+	}
+
+}
