851d05c172dfc9668e5fa7b61ea4065c3e4062b9
prince-menaria <princemenaria04@gmail.com>
2022-12-13 10:48:55 +0530

    initial commit

---
+++ src/basicProgram/sum.java
@@ -0,0 +1,18 @@
+package basicProgram;
+
+public class sum {
+	
+	
+	
+	 
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c;
+		
+		c = a+b;
+		System.out.println(c);
+		
+	}
+
+}
